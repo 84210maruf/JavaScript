d34f6e5ae959039f7f2fd48b5e113171f9402fd5
maruf <maruf84210cse@gmail.com>
2021-05-01 23:08:33 +0600

    add Code text

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-console.log("Hallow me HAA!");
-//alert('me');
+
+let name = "maruf";
+console.log("My Name Is "+name);
+
+let firstName = "Maruf";
+let lastName = "Hossen";
+console.log(firstName+" "+lastName);
+
+// It's Worked
+let f = "Maruf";
+f = "Hossen";
+console.log(f);
+
+// It's not work
+// const mean Constant value
+const c = 19;
+console.log(c);
+
+// Initialization
+let name1 = 'Maruf';
+let age = 30;
+let isApproved = false;
+let firstName1 = undefined;
+let selectedColor = null;
+
+console.log(typeof(name1))
