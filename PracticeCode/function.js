6892353b253c01fb1ded0cefa9dad7e5083ca216
maruf <maruf84210cse@gmail.com>
2021-05-04 19:17:12 +0600

    stable version

---
+++ PracticeCode/function.js
@@ -0,0 +1,20 @@
+
+//Function type 01
+function a(){
+    return 1;
+}
+
+// Function type 02
+const  learn = function(){
+    return 2;
+}
+console.log(learn());
+
+// Array Function
+const learn = (num) => {
+    return "Hellow"+num;
+}
+//OR array func
+const learn = (num) => "Hellow"+num;
+
+
