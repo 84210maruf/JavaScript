8edc9c9e37d64845b035d9a6521464b6f02995b7
maruf <maruf84210cse@gmail.com>
2021-05-04 16:58:57 +0600

    from VisualStudio

---
+++ PracticeCode/index.js
@@ -0,0 +1,32 @@
+
+//Function type 01
+function a(){
+    return 1;
+}
+
+// Function type 02
+const  learn = function(){
+    return 2;
+}
+console.log(learn());
+
+// Array Function
+const learn = (num) => {
+    return "Hellow"+num;
+}
+//OR array func
+const learn = (num) => "Hellow"+num;
+
+// Import & Export
+
+//In test.js file
+//    const testing = () => {
+//        console.log("testing");
+//    }
+//  export testing;
+
+//In app.js file
+//  import {test} from './test'
+
+
+
