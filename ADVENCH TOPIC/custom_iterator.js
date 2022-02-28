16cb623af80b2b1b481ef350f5f88dd5cc5e1ad5
maruf <maruf84210cse@gmail.com>
2022-02-28 11:58:09 +0600

    Prectice Async Generator

--- ADVENCH TOPIC/custom_iterator.js
+++ ADVENCH TOPIC/custom_iterator.js
@@ -1,3 +1,5 @@
+// custom iterator...
+
 function range(start, end, step){
     let current = start;
     return{
@@ -32,3 +34,38 @@ const iterator = rang[Symbol.iterator]();
 console.log(iterator.next());
 console.log(iterator.next().value);
 console.log(iterator.next().done);
+
+//Create Object iterable
+
+let object = {
+    value1: 1,
+    value2: 2,
+    value3: 3,
+    value4: 4
+}
+
+Object.prototype[Symbol.iterator] = function(){
+    const entries = Object.entries(object);
+    let count = entries.length;
+    let index = 0;
+    return {
+        next() {
+            if(count > 0){
+                let result = {
+                    done: false,
+                    value: entries[index][1]
+                }
+                count--;
+                index++;
+                return result;
+            }
+            return {done: true}
+        }
+    }
+};
+
+console.log([...object]);
+
+for(let element of object){
+    console.log(element)
+}
